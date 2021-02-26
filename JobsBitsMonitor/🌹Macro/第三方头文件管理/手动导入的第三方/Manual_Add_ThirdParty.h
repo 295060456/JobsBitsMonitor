67900563442a9e2b77ddb7d9b3132ffbb794d48d
Jobs <lg295060456@gmail.com>
2021-02-26 19:52:26 +0700

    引入了CallBackInfoByBlock分类，作用于View 和 VC，但是还没有全局部署

--- JobsBitsMonitor/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
+++ JobsBitsMonitor/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
@@ -69,6 +69,7 @@ alpha:1] \
 #import "UIView+EmptyData.h"
 #import "UIView+Gesture.h"
 #import "UIView+ViewController.h"
+#import "UIView+CallBackInfoByBlock.h"
 #pragma mark —— NSString
 #import "NSString+Extras.h"
 #pragma mark —— NSObject
