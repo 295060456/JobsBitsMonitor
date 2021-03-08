d060f39c86d6826331bf4e88597e6f1b0385c999
Jobs <lg295060456@gmail.com>
2021-03-08 18:13:16 +0700

    no message

--- JobsBitsMonitor/3rd/Manual_Add_ThirdParty/CheckMemFreeVC/CheckMemFreeVC.h
+++ JobsBitsMonitor/3rd/Manual_Add_ThirdParty/CheckMemFreeVC/CheckMemFreeVC.h
@@ -5,11 +5,12 @@
 //  Created by Jobs on 2021/3/5.
 //
 
-#import "BaseViewController.h"
+#import <UIKit/UIKit.h>
+#import "WHToast+DDToast.h"
 
 NS_ASSUME_NONNULL_BEGIN
 /// 本类用来测试是否正常走deallco方法的控制器
-@interface CheckMemFreeVC : BaseViewController
+@interface CheckMemFreeVC : UIViewController
 
 @end
 
